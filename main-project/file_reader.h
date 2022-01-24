967fea35c8c3f86a487b38f7582f4e023974474a
Kirill <KirillSidler@gmail.com>
2022-01-24 18:51:35 +0300

    Реализованно чтение данных из файла

---
+++ main-project/file_reader.h
@@ -0,0 +1,8 @@
+#ifndef FILE_READER_H
+#define FILE_READER_H
+
+#include "telephone_subscription.h"
+
+void read(const char* file_name, telephone_subscription* array[], int& size);
+
+#endif#pragma once#pragma once
--- main-project/main.cpp
+++ main-project/main.cpp
@@ -2,10 +2,60 @@
 
 using namespace std;
 
+#include "telephone_subscription.h"
+#include "file_reader.h"
+#include "constants.h"
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     cout << "Laboratory work #1. GIT\n";
     cout << "Variant #9. Telephone speaks\n";
     cout << "Author: Kirill Radzko\n";
+    telephone_subscription* subscriptions[MAX_FILE_ROWS_COUNT];
+    int size;
+    try
+    {
+        read("data.txt", subscriptions, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Телефон........: ";
+            cout << subscriptions[i]->telephone << '\n';
+
+            cout << "Дата звонка.....: ";
+            cout << setw(4) << setfill('0') << subscriptions[i]->call.year << '-';
+
+            cout << setw(2) << setfill('0') << subscriptions[i]->call.month << '-';
+
+            cout << setw(2) << setfill('0') << subscriptions[i]->call.day;
+            cout << '\n';
+
+            cout << "Время начала........: ";
+            cout << subscriptions[i]->start.hours << ' ';
+            cout << subscriptions[i]->start.min << ' ';
+            cout << subscriptions[i]->start.sec << '\n';
+
+            cout << "Время конца........: ";
+            cout << subscriptions[i]->end.hours << ' ';
+            cout << subscriptions[i]->end.min << ' ';
+            cout << subscriptions[i]->end.sec << '\n';
+
+            cout << "Тариф........: ";
+            cout << subscriptions[i]->tarif << '\n';
+
+            cout << "Cтоимость........: ";
+            cout << subscriptions[i]->money << '\n';
+
+            cout << '\n';
+        }
+        for (int i = 0; i < size; i++)
+        {
+            delete subscriptions[i];
+        }
+    }
+    catch (const char* error)
+    {
+        cout << error << '\n';
+    }
     return 0;
 }
