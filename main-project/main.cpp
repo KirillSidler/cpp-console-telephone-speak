1d3a34c2ff882236108870657cf4b0d206c9d0d3
Kirill <KirillSidler@gmail.com>
2022-01-24 18:59:29 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #1. GIT\n";
-    cout << "Variant #9. Telephone speaks\n";
+    cout << "Variant #9. Telephone Speaks\n";
     cout << "Author: Kirill Radzko\n";
+    cout << "Group: 14\n";
     return 0;
 }
